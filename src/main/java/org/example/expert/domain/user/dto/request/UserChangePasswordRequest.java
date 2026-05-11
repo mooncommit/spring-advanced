0fd57c3c3834cf5ac12df2551907cd49500f1891
mooncommit <mooncommit@users.noreply.github.com>
2026-05-11 10:34:18 +0900

    레벨 2-3 : Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,9 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[A-Z]).{8,}$", // (?=.*\d) -> 숫자 하나 이상 포함, (?=.*[A-Z]) -> 대문자 하나 이상 포함, .{8,} -> 전체 길이 8자 이상
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."
+    )
     private String newPassword;
 }
